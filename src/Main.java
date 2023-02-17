b5228b59beb1f3884e19a14c80a34d7ebe1e263f
Kamal Kouidar <kamal.kouidar@hva.nl>
2023-02-17 22:42:22 +0100

    2 soorten verbind mogelijkheden gemaakt! fysiek of virtuele robot

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,13 @@ import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String robotUrl = "tcp://nao.local:9559";
-        // Create a new application
-        Application application = new Application(args, robotUrl);
-        // Start your application
-        application.start();
+        Robottesten nao = new Robottesten();
         // Create an ALTextToSpeech object and link it to your current session
-        ALTextToSpeech tts = new ALTextToSpeech(application.session());
+//        ALTextToSpeech tts = new ALTextToSpeech(NAO.session());
         // Make your robot say something
-        tts.say("Hello Mats!");
+//        tts.say("Hello Mats!");
+        nao.verbinden("localhost",50155);   // verbinden met de virtuele robot in choreograph
+//        nao.fysiekVerbinden();                         // verbinden met onze fysieke robot
+        nao.zeg("");
     }
 }
--- src/Robottesten.java
+++ src/Robottesten.java
@@ -1,19 +1,40 @@
 package src;
 
+import com.aldebaran.qi.Application;
+import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+
+
 public class Robottesten {
+
     private String naam;
 
-    public void verbinden(){
+    private Application application;
 
+    public void verbinden(String hostname, int port) {
+        String robotUrl = "tcp://" + hostname + ":" + port;    // Create a new application
+        this.application = new Application(new String[]{}, robotUrl);
+        // Start your application
+        application.start();
     }
 
-    public void staan(){
-
+    public void fysiekVerbinden() {
+        String robotUrl = "tcp://padrick.local:" +  9559;    // Create a new application
+        this.application = new Application(new String[]{}, robotUrl);
+        // Start your application
+        application.start();
     }
 
-    public void zitten(){
 
+
+    public void zeg(String tekst) throws Exception {
+        // Create an ALTextToSpeech object and link it to your current session
+        ALTextToSpeech tts = new ALTextToSpeech(this.application.session());
+        // Make your robot say something
+        tts.say(tekst);
     }
+
+
+
 
 
 }
