c6c72dbb918276a62bbb50d6adc8054957d5ea5b
Kamal Kouidar <kamal.kouidar@hva.nl>
2023-02-17 15:56:49 +0100

    tutorial + methodes toegevoegd

--- src/Robottesten.java
+++ src/Robottesten.java
@@ -1,4 +1,19 @@
 package src;
 
 public class Robottesten {
+    private String naam;
+
+    public void verbinden(){
+
+    }
+
+    public void staan(){
+
+    }
+
+    public void zitten(){
+
+    }
+
+
 }
