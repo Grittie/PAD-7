8126bf151cdf003b7c4556ab3d03190309fcf387
Sem Wester <sem.wester@hva.nl>
2023-02-22 15:35:11 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,10 @@ public class Main {
 //        ALTextToSpeech tts = new ALTextToSpeech(NAO.session());
         // Make your robot say something
 //        tts.say("Hello Mats!");
-//        nao.verbinden("localhost",55470);   // verbinden met de virtuele robot in choreograph
+        //nao.verbinden("localhost",55470);   // verbinden met de virtuele robot in choreograph
         nao.fysiekVerbinden();
 // verbinden met onze fysieke robot
-//        Thread.sleep(2000);
-        nao.zeg("Op een vrijdag in de kroeg\n" +
-                "Ergens in Amsterdam\n" +
-                "Zat aan de bar met een glas een oude wijze man\n" +
-                "Hij zei dat die nog maar een paar dagen had\n" +
-                "Dus pak het leven, pak alles en ga er mee op pad\n" +
-                "En hij zei\n" +
-                "\"Leef, alsof het je laatste dag is\"\n" +
-                "\"Leef, alsof de morgen niet bestaat\"\n" +
-                "\"Leef, alsof het nooit echt af is\"\n" +
-                "En \"leef, pak alles wat je kan");
+        nao.zeg("Lars irritant kutkind");
 
     }
 }
