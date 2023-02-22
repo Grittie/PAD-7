83e412c9baa0b32f550a0b639197928e00a9d8d9
Kamal Kouidar <kamal.kouidar@hva.nl>
2023-02-22 15:43:19 +0100

    Main.java updated

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 package src;
 
-import com.aldebaran.qi.Application;
-import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Robottesten nao = new Robottesten();
-        // Create an ALTextToSpeech object and link it to your current session
-//        ALTextToSpeech tts = new ALTextToSpeech(NAO.session());
-        // Make your robot say something
-//        tts.say("Hello Mats!");
         //nao.verbinden("localhost",55470);   // verbinden met de virtuele robot in choreograph
         nao.fysiekVerbinden();
-// verbinden met onze fysieke robot
-        nao.zeg("Op een vrijdag in de kroeg\n" +
-                "Ergens in Amsterdam\n");
+        // verbinden met onze fysieke robot
+        nao.zeg("Op een vrijdag in de kroeg");
 
     }
 }
