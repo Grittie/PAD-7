f7dd3d6deafd47e2238dd5425aa847534c082b07
Toine Donker <toine.donker@hva.nl>
2023-03-07 17:14:24 +0100

    Add boilerplate code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 package src;
 
-import com.aldebaran.qi.Application;
-import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String robotUrl = "tcp://nao.local:9559";
-        // Create a new application
-        Application application = new Application(args, robotUrl);
-        // Start your application
-        application.start();
-        // Create an ALTextToSpeech object and link it to your current session
-        ALTextToSpeech tts = new ALTextToSpeech(application.session());
-        // Make your robot say something
-        tts.say("Hello Mats!");
+        NAO nao = new NAO();
+        nao.connect("nao.local", 9559);
     }
 }
--- src/NAO.java
+++ src/NAO.java
@@ -1,19 +1,36 @@
 package src;
 
+import com.aldebaran.qi.Application;
+import com.aldebaran.qi.helper.proxies.ALRobotPosture;
+import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+
 public class NAO {
-    private String naam;
+    private String name;
+    private Application application;
 
-    public void verbinden(){
-
-    }
-
-    public void staan(){
-
-    }
-
-    public void zitten(){
+    public void connect(String hostname, int port) {
+        String robotUrl = "tcp://" + hostname + ":" + port;
+        // Create a new application
+        this.application = new Application(new String[] {}, robotUrl);
+        // Start your application
+        application.start();
 
     }
 
+    public void stand() throws Exception {
+        ALRobotPosture robotPosture = new ALRobotPosture(application.session());
+        robotPosture.goToPosture("StandInit", 0.5f);
+    }
 
+    public void sit() throws Exception {
+        ALRobotPosture robotPosture = new ALRobotPosture(application.session());
+        robotPosture.goToPosture("StandInit", 0.5f);
+    }
+
+    public void say(String tekst) throws Exception {
+        // Create an ALTextToSpeech object and link it to your current session
+        ALTextToSpeech tts = new ALTextToSpeech(this.application.session());
+        // Make your robot say something
+        tts.say(tekst);
+    }
 }
