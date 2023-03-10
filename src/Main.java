24faa030266eebe878ab1cffa455ba14dcf0d00a
Lars Grit <lars.grit@hva.nl>
2023-03-10 14:32:58 +0100

    main fix

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,5 @@ package src;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Robottesten nao = new Robottesten();
-        //nao.verbinden("localhost",55470);   // verbinden met de virtuele robot in choreograph
-        nao.fysiekVerbinden();
-        // verbinden met onze fysieke robot
-        nao.zeg("Op een vrijdag in de kroeg");
-
     }
 }
