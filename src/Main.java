3e56d080da55969cc3d03dcfdd94ebedfe04a0d0
@gritla <lars.grit@hva.nl>
2023-03-10 14:33:50 +0100

    removing nao file

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,5 @@ package src;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        NAO nao = new NAO();
-        Dab dab = new Dab();
-//        nao.verbinden("nao.local.",9559);   // verbinden met de virtuele robot in choreograph
-        nao.verbinden("localhost",53130);   // verbinden met de virtuele robot in choreograph
-//        nao.fysiekVerbinden();
-        nao.zeg("Op een vrijdag in de kroeg");
-        dab.dab();
-        Thread.sleep(100);
     }
 }
