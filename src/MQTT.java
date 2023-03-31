26809e61fed7ed34212d73294b5ba596bf0fd601
@gritla <lars.grit@hva.nl>
2023-03-31 12:43:06 +0200

    getting ready for merge

--- src/MQTT.java
+++ src/MQTT.java
@@ -3,18 +3,22 @@ package src;
 import org.eclipse.paho.client.mqttv3.*;
 
 public class MQTT {
+    // MQTT broker credentials
     public static String MQTT_HOST = "tcp://mqtt.hva-robots.nl:1883";
     public static String MQTT_CLIENT_ID = "gritla_0";
     public static String MQTT_USERNAME = "gritla";
     public static String MQTT_PASSWORD = "D6G9E1b95x8h3LaGFtxA";
 
+    // Function that runs the MQTT Client
     public static void mqttClient() throws Exception {
+        // Connects to MQTT broker
         MqttClient client = new MqttClient (MQTT_HOST, MQTT_CLIENT_ID);
         MqttConnectOptions connectOptions = new MqttConnectOptions();
         connectOptions.setUserName (MQTT_USERNAME);
         connectOptions.setPassword (MQTT_PASSWORD.toCharArray());
         client.connect(connectOptions);
 
+        // Prints true if connection is made
         System.out.print("Verbonden? ");
         System.out.println(client.isConnected());
 
@@ -24,6 +28,7 @@ public class MQTT {
                 System.out.println("connection lost...");
             }
 
+            // Listens to arrived messages and prints the topic and message
             @Override
             public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
                 System.out.println("Bericht ontvangen");
@@ -32,6 +37,7 @@ public class MQTT {
                 System.out.print("Bericht: ");
                 System.out.println(mqttMessage.toString());
 
+                // Switch case that plays presentation according to buttong pressed on website
                 switch (mqttMessage.toString()) {
                     case "SE":
                         System.out.println("Software engineering presentation starting...");
@@ -57,6 +63,7 @@ public class MQTT {
             }
         });
 
+        // Subscribed to topic
         client.subscribe("gritla/leerroute");
     }
 }
