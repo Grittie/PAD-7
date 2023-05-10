a485b2eeffee6d7892c66683942f02872ccef9d6
Kamal Kouidar <kamal.kouidar@hva.nl>
2023-05-10 18:17:41 +0200

    Create an image for displaying results as a bar diagram CreateImage.java

--- src/CreateImage.java
+++ src/CreateImage.java
@@ -8,43 +8,37 @@ import java.io.IOException;
 
 public class CreateImage {
 
-    public void rectangle(String onderwerp, int witdh, int y, Graphics2D graphics2D) {
-        graphics2D.setColor(Color.black);
-        graphics2D.drawString(onderwerp, 20, y);
-        graphics2D.drawRect(50, 50, witdh, 20);
-        graphics2D.fillRect(50, 50, witdh, 20);
+    // Create Rectangle
+    public void rectangle(String onderwerp, Color color, int witdh, int y, Graphics2D graphics2D, int i) {
+        graphics2D.setColor(color);
+        graphics2D.drawString(onderwerp, 25, y);
+        graphics2D.drawRect(200, y -13, witdh, 20);
+        graphics2D.fillRect(200, y -13, witdh, 20);
 
     }
 
     public void staafDiagram() throws IOException {
-        int width = 300;
-        int height = 300;
-
+        int width = 400;
+        int height = 200;
+        int y = 50;
 
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
         Graphics2D graphics2D = bufferedImage.createGraphics();
 
-        String[] onderwerp = {"Software-Engineering", "BIM"};
-        int[] results = {140, 150};
-
+        String[] onderwerp = {"Software-Engineering:", "Scrum-master:", "IT-ondernemer:", "Lars:", "Luka:"};
+        int[] results = {140, 150, 100, 80, 90}; // results omrekenen voor juiste staafbreedte
+        Color[] kleuren = {Color.BLUE, Color.orange, Color.RED, Color.green, Color.yellow};
 
         graphics2D.setColor(Color.white);
         graphics2D.fillRect(0, 0, width, height);
 
-//        graphics2D.setColor(Color.black);
-//        graphics2D.drawString("1.", 20, 60);                // Y + 50
-//        graphics2D.drawRect(50, 50, score2, 20);
-//        graphics2D.fillRect(50, 50, 140, 20);
-//
-//        graphics2D.setColor(Color.black);
-//        graphics2D.drawString("2.", 20, 110);
-//        graphics2D.drawRect(50, 100, score1, 20);
-//
-        int y = 50;
+        CreateImage imageCreator = new CreateImage();
+
+
         for (int i = 0; i < onderwerp.length; i++) {
-            this.rectangle(onderwerp[i],results[i],y, graphics2D);
-            y += 50;
+            imageCreator.rectangle(onderwerp[i], kleuren[i], results[i], y, graphics2D, i);
+            y += 30;
         }
 
         graphics2D.dispose();
