fecb57c6bd9c743ff063e08b16711d110c11d23c
@gritla <lars.grit@hva.nl>
2023-05-10 19:24:11 +0200

    fixing casting errors and putting all answers in array to send to the image gneerator

--- src/Questions.java
+++ src/Questions.java
@@ -14,7 +14,7 @@ public class Questions {
     static private ALTextToSpeech textToSpeech;
     static private JSONParser parser;
     static private MQTT mqtt;
-    static private int[] score = new int[5];
+    static private long[] score = new long[5];
     static private boolean isPressed = false;
 
     static private ArrayList answers;
@@ -85,34 +85,35 @@ public class Questions {
                     case "Yes":
                         // System.out.println(answers.get(0));
                         System.out.println(((JSONObject) answers.get(0)).get("score-back-end"));
-                        score[0] = (int) ((JSONObject) answers.get(0)).get("score-back-end");
-                        // score[1] = (int) ((JSONObject) answers.get(0)).get("score-front-end");
-                        // score[2] = (int) ((JSONObject) answers.get(0)).get("score-robot-ui");
-                        // score[3] = (int) ((JSONObject) answers.get(0)).get("score-robot-technical");
-                        // score[4] = (int) ((JSONObject) answers.get(0)).get("score-ict-ondernemer");
+                         score[0] = (int) (long) ((JSONObject) answers.get(0)).get("score-back-end");
+                         score[1] = (int) (long) ((JSONObject) answers.get(0)).get("score-front-end");
+                         score[2] = (int) (long) ((JSONObject) answers.get(0)).get("score-robot-ui");
+                         score[3] = (int) (long) ((JSONObject) answers.get(0)).get("score-robot-technical");
+                         score[4] = (int) (long) ((JSONObject) answers.get(0)).get("score-ict-ondernemer");
 
                         break;
                     case "Maybe":
                         System.out.println(answers.get(1));
 
-                        // score[0] = (int) ((JSONObject) answers.get(1)).get("score-back-end");
-                        // score[1] = (int) ((JSONObject) answers.get(1)).get("score-front-end");
-                        // score[2] = (int) ((JSONObject) answers.get(1)).get("score-robot-ui");
-                        // score[3] = (int) ((JSONObject) answers.get(1)).get("score-robot-technical");
-                        // score[4] = (int) ((JSONObject) answers.get(1)).get("score-ict-ondernemer");
+                         score[0] = (int) (long) ((JSONObject) answers.get(1)).get("score-back-end");
+                         score[1] = (int) (long) ((JSONObject) answers.get(1)).get("score-front-end");
+                         score[2] = (int) (long) ((JSONObject) answers.get(1)).get("score-robot-ui");
+                         score[3] = (int) (long) ((JSONObject) answers.get(1)).get("score-robot-technical");
+                         score[4] = (int) (long) ((JSONObject) answers.get(1)).get("score-ict-ondernemer");
 
                         break;
                     case "No":
                         System.out.println(answers.get(2));
 
-                        // score[0] = (int) ((JSONObject) answers.get(2)).get("score-back-end");
-                        // score[1] = (int) ((JSONObject) answers.get(2)).get("score-front-end");
-                        // score[2] = (int) ((JSONObject) answers.get(2)).get("score-robot-ui");
-                        // score[3] = (int) ((JSONObject) answers.get(2)).get("score-robot-technical");
-                        // score[4] = (int) ((JSONObject) answers.get(2)).get("score-ict-ondernemer");
+                         score[0] = (int) (long) ((JSONObject) answers.get(2)).get("score-back-end");
+                         score[1] = (int) (long) ((JSONObject) answers.get(2)).get("score-front-end");
+                         score[2] = (int) (long) ((JSONObject) answers.get(2)).get("score-robot-ui");
+                         score[3] = (int) (long) ((JSONObject) answers.get(2)).get("score-robot-technical");
+                         score[4] = (int) (long) ((JSONObject) answers.get(2)).get("score-ict-ondernemer");
 
                         break;
                     default:
+                        System.out.println("This is not a valid input");
                         break;
                 }
 
@@ -158,6 +159,8 @@ public class Questions {
                 isPressed = false;
             }
 
+            System.out.println("closing");
+            System.out.println(Arrays.toString(score));
             MQTT.client.close();
             // TODO: data doorsturen(run fuctie van Kamal)
         } catch (Exception e) {
