6d4c31e08438103f38103c7f7ccb01122365f3b4
@gritla <lars.grit@hva.nl>
2023-05-12 13:13:57 +0200

    latest changes

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         Questions questions = new Questions();
         questions.askAllQuestions();
