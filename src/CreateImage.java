9b63801dd2e989b4bf88c578f55e56da3109b125
Kamal Kouidar <kamal.kouidar@hva.nl>
2023-05-19 02:12:08 +0200

    add code for sorting the results in the bar diagram

--- src/CreateImage.java
+++ src/CreateImage.java
@@ -1,3 +1,4 @@
+
 package src;
 
 import javax.imageio.ImageIO;
@@ -6,46 +7,68 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class CreateImage {
 
-    // Create Rectangle
     public void rectangle(String onderwerp, Color color, int witdh, int y, Graphics2D graphics2D, int i) {
         graphics2D.setColor(color);
         graphics2D.drawString(onderwerp, 25, y);
-        graphics2D.drawRect(200, y -13, witdh, 20);
-        graphics2D.fillRect(200, y -13, witdh, 20);
-
+        graphics2D.drawRect(200, y - 13, witdh, 20);
+        graphics2D.fillRect(200, y - 13, witdh, 20);
     }
 
     public void staafDiagram(long[] score) throws IOException {
-        System.out.println(Arrays.toString(score));
 
-        int width = 400;
-        int height = 200;
+        // Dimensions of the background of the image
+        int width = 500;
+        int height = 1000;
+
+        //Vertical position of the rectangles
         int y = 50;
 
+        // Maak een nieuw BufferedImage
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
+        // Maak een Graphics2D-object van het BufferedImage
         Graphics2D graphics2D = bufferedImage.createGraphics();
 
+        // Onderwerpen en kleuren voor de rechthoeken
         String[] onderwerp = {"Back-end Engineer:", "Front-end Engineer:", "Robot UI:", "Robot Technical:", "ICT Ondernemer:"};
         Color[] kleuren = {Color.BLUE, Color.orange, Color.RED, Color.green, Color.magenta};
 
+        // Stel de achtergrondkleur van het BufferedImage in op wit
         graphics2D.setColor(Color.white);
         graphics2D.fillRect(0, 0, width, height);
 
+        // Maak een instantie van CreateImage
         CreateImage imageCreator = new CreateImage();
 
+        // Sorteer de score array van hoog naar laag
+        Integer[] sortedIndexes = new Integer[score.length];
+        for (int i = 0; i < score.length; i++) {
+            sortedIndexes[i] = i; //elke index i van de sortedIndices-array gevuld met dezelfde waarde als de oorspronkelijke index van de score-array.
+        }
 
+        Arrays.sort(sortedIndexes, Comparator.comparingLong(i -> -score[i]));
+
+        // Teken de rechthoeken in de gesorteerde volgorde
         for (int i = 0; i < onderwerp.length; i++) {
-            imageCreator.rectangle(onderwerp[i], kleuren[i], (int) score[i], y, graphics2D, i);
+            int index = sortedIndexes[i];
+            // Teken rechthoek
+            imageCreator.rectangle(onderwerp[index], kleuren[index], (int) score[index], y, graphics2D, i);
             y += 30;
         }
 
+        // Maak de Graphics2D schoon
         graphics2D.dispose();
 
+        // Bestand waarin de afbeelding wordt opgeslagen
         File file = new File("./QRcode/QRcodes/results/result.png");
+
+        // Schrijf het BufferedImage naar het bestand als een PNG-afbeelding
         ImageIO.write(bufferedImage, "png", file);
     }
+
+
 }
