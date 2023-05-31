444bef27bdccaf415a275c9ae33ebff200ebb68b
Kamal Kouidar <kamal.kouidar@hva.nl>
2023-05-31 15:30:01 +0200

    changed Results board + backgroundimage and nao photo

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ package src;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        NAO nao = new NAO("johanus.local", 9559);
-        nao.staan();
-        nao.say("Hallo ik ben de Studie Keuze bot. Ik zal jullie een korte demonstratie geven van een studie keuze check.");
-        Thread.sleep(1000);
-        nao.say("Ik zal je een paar vragen stellen. Jij kan hier op antwoorden met Ja, Nay of Misschien door te drukken op de knoppen voor je!");
-        nao.say("Daarna zal ik een QR code maken met mijn advies die je dan met je telefoon kan scannen!");
-        Thread.sleep(1000);
-        Questions questions = new Questions(nao);
-        questions.askAllQuestions();
+//        NAO nao = new NAO("johanus.local", 9559);
+//        nao.staan();
+//        nao.say("Hallo ik ben de Studie Keuze bot. Ik zal jullie een korte demonstratie geven van een studie keuze check.");
+//        Thread.sleep(1000);
+//        nao.say("Ik zal je een paar vragen stellen. Jij kan hier op antwoorden met Ja, Nay of Misschien door te drukken op de knoppen voor je!");
+//        nao.say("Daarna zal ik een QR code maken met mijn advies die je dan met je telefoon kan scannen!");
+//        Thread.sleep(1000);
+//        Questions questions = new Questions(nao);
+//        questions.askAllQuestions();
+
+        long[] scores = {200,150,100,50, 20};
+        CreateImage createImage = new CreateImage();
+        createImage.barChart(scores);
     }
 }
--- src/Questions.java
+++ src/Questions.java
@@ -152,7 +152,7 @@ public class Questions {
 
             // Send score array to CreateImage class
             CreateImage createImage = new CreateImage();
-            createImage.staafDiagram(score);
+            createImage.barChart(score);
 
             System.out.println("closing");
 
