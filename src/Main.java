010bb3b868c00e810bffc4177d9b88c16cab7946
@gritla <lars.grit@hva.nl>
2023-06-05 12:46:05 +0200

    changed the code to order the results and add a index

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,19 @@ import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scores scores = new Scores();
+        scores.getResults();
 
-        NAO nao = new NAO("169.254.130.214", 9559);
-        while (true) {
-            Introduction introduction = new Introduction(nao);
-            introduction.introductionSequence();
-
-            Questions questions = new Questions(nao);
-            questions.askAllQuestions();
-
-            Scores scores = new Scores();
-            scores.getResults();
-        }
+//        NAO nao = new NAO("169.254.130.214", 9559);
+//        while (true) {
+//            Introduction introduction = new Introduction(nao);
+//            introduction.introductionSequence();
+//
+//            Questions questions = new Questions(nao);
+//            questions.askAllQuestions();
+//
+//            Scores scores = new Scores();
+//            scores.getResults();
+//        }
     }
 }
