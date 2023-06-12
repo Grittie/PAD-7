f0e69dd92ba80abd0f88a151b1a0ade631f10cb3
Toine Donker <toine.donker@hva.nl>
2023-06-12 15:27:32 +0200

    Remove unnecessary imports in `main`

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.awt.desktop.QuitEvent;
-import java.io.FileReader;
-
 public class Main {
     public static void main(String[] args) throws Exception {
 
